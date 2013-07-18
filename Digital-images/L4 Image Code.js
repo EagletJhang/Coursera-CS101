fa51c0a9df8175be432a961bf74112f59305e000
EagletJhang <eagletblueblue@gmail.com>
2013-07-18 23:21:42 +0800

    Update and rename L4 Image Code.md to L4 Image Code.js

--- Digital-images/L4 Image Code.js
+++ Digital-images/L4 Image Code.js
@@ -1,12 +1,8 @@
 //pixel image 二者可自定義
 
 image = new SimpleImage("x.png");
-
 image.setZoom(20);//放大
-
 pixel = image.getPixel(0, 0);// upper left pixel
-
 pixel.setRed(255); //set red value of that pixel
-
 print(image);
 
