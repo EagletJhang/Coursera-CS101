7df9d6bc833a516f3ef6bba52a41b13163fd17e1
EagletJhang <eagletblueblue@gmail.com>
2013-07-18 23:23:46 +0800

    Update and rename L5 For Loops.md to L5 For Loops.js

--- Digital-images/L5 For Loops.js
+++ Digital-images/L5 For Loops.js
@@ -1,22 +1,12 @@
 image = new SimpleImage("flowers.jpg");
-
 for (pixel: image) {
-
 pixel.setRed(0);
-
 pixel.setGreen(0);
-
 pixel.setBlue(0);
-
 } 
 //body of the loops
-
 print(image);
 
-------------------
 
-Note: Javascript does not have this feature, I added it for CS101
-
-------------------
-
-Loop: powerful, but mechanical
+//Note: Javascript does not have this feature, I added it for CS101
+//Loop: powerful, but mechanical
