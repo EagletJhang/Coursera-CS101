5e433e41ddd872178464a4da886082820861afdb
EagletJhang <eagletblueblue@gmail.com>
2013-07-18 23:29:48 +0800

    Update L9 If Logic.js

--- Digital-images/L9 If Logic.js
+++ Digital-images/L9 If Logic.js
@@ -1,19 +1,12 @@
 image = new SimpleImage("flowers.jpg");
-
 for (pixel: image) {
-
 if (pixel.getRed() > 160) {
 
 //test
 
 pixel.setRed(0);
-
 pixel.setGreen(0);
-
 pixel.setBlue(0);
-
 }
-
 }
-
 print(image);
