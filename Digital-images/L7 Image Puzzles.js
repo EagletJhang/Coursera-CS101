4957b5fdd75f629f8ae356c11f55eede77c39950
EagletJhang <eagletblueblue@gmail.com>
2013-07-18 23:27:25 +0800

    Update and rename L7 Image Puzzles.md to L7 Image Puzzles.js

---
+++ Digital-images/L7 Image Puzzles.js
@@ -0,0 +1,5 @@
+pixel.setGreen(0);
+pixel.setBlue(0);
+// Strategy: zero out blue and green as they are just garbage data.
+pixel.setRed(pixel.getRed() * 10);
+// Then scale red up by 10x to see the real image in red.
