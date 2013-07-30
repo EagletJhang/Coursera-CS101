89c969eb56a8dab595e4cee563b3c7182835b2dd
Eagelt Jhang <eagletblueblue@gmail.com>
2013-07-30 20:40:54 +0800

    Create L18 Table 1.js

---
+++ Tables/L18 Table 1.js
@@ -0,0 +1,17 @@
+table = new SimpleTable("baby-2010.csv");
+
+for (row: table) {
+  print(row);
+}
+
+//Table Query Logic 查询
+table = new SimpleTable("baby-2010.csv");
+for (row: table) {
+  if (row.getField("rank") == 6) {
+    print(row);
+  }
+}
+
+//rank field is < 10
+//rank field is <= 10
+//rank field is > 990
